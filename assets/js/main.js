8105a5689aca188658de3e52fb29e8d870e7e9d4
Ganjali717 <ganjaliki@code.edu.az>
2021-08-31 12:39:57 +0400

    6. Today i create in global-employment section transitions and responsive of homepage

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ let itworks = document.querySelector('#howitwork');
 let headresources = document.querySelector('#headresources');
 let dropdownmenu = document.querySelector('.dropdown-menu-header');
 let seconddropdownmenu = document.querySelector('.second-dropdown-menu-header');
-let dropdownicon = document.querySelector('bi bi-arrow-down-short');
+let dropdownicon = document.querySelector('.dropdown-icon');
 
 itworks.addEventListener('click', function(){ 
     dropdownmenu.classList.add("active-dropdown");
@@ -11,6 +11,29 @@ itworks.addEventListener('click', function(){
 });
 headresources.addEventListener('click', function(){ 
     seconddropdownmenu.classList.add("active-dropdown");
+    dropdownicon.classList.add("open");
+});
+
+
+//Cards-love
+const workersimg = Array.from(document.querySelectorAll('.workers-img .worker'));
+const workerscomment = Array.from(document.querySelectorAll('.workers-comments .workers-comments-item'));
+const clearActives = () => {
+    workersimg.forEach(workersimg => {
+        workersimg.classList.remove('active');
+    });
+    workerscomment.forEach(workerscomment => {
+        workerscomment.classList.remove('active');
+    });
+}
+workersimg.forEach(workersimg => {
+    workersimg.onclick = function () {
+        clearActives();
+        const targetId = workersimg.getAttribute('data-target') //home
+        const targetContent = document.getElementById(targetId);
+        workersimg.classList.add('active');
+        targetContent.classList.add('active');
+    }
 });
 
 
